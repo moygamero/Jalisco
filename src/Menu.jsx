9ae70f73d11b729b2bbbbdf1e84a44f8aaf3794c
Alix Rodriguez <alix.rodriguez@jirehpro.co>
2023-07-21 22:38:37 -0400

    Prueba de imagen

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
     }
 
     const backgroundStyle = {
-        backgroundImage: 'url("/src/assets/5.jpg")',
+        backgroundImage: 'url("./src/assets/5.jpg")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         height: '100vh'
@@ -30,7 +30,7 @@ function Menu() {
             <div style={backgroundStyle} className='margarita flex flex-col items-center justify-end'>
                 <div>
                     <div className="flex  justify-center items-center">
-                        <button style={btn1} className="py-2 px-4  btn-hover rounded-full m-2">DRINKS</button>
+                        <button style={btn1} className="py-2 px-4 btn-hover rounded-full m-2">DRINKS</button>
                     </div>
                 </div>
                 <p>Viva Jalisco Restaurant in Bothell is renowned for crafting the most exquisite Margaritas in town. With their blended flavors and top-quality ingredients, their Margaritas are truly a taste of perfection.</p>
